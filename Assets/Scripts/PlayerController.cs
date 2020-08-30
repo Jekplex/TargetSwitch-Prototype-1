485af16b6612738e36c6a69f8c68942067eb5100
Jekplex <41015593+Jekplex@users.noreply.github.com>
2020-08-30 11:55:31 +0100

    Experimentation with new input system
    Trying to figure out how to approach handling player input. Might revert to something simplier.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        
+        var moveDirection = context.ReadValue<Vector2>();
+
+        transform.position += new Vector3(moveDirection.x, moveDirection.y) * moveSpeed * Time.deltaTime;
+
+    }
+
+}
