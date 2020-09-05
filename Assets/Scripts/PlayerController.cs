bc86d18cfeff383c1717e9d5170362f9b7923c22
Jekplex <41015593+Jekplex@users.noreply.github.com>
2020-09-05 13:40:17 +0100

    Revised input handling
    New input system doesn't have continuous input on mouse and keyboard. So player is required to spam WASD - which is not what I want. The only way I see to bypass this issue is by hand coding all inputs through this script. (Player Controller). Potentially reducing all input devices strictly to mouse and keyboard.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,16 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    //public InputAction myInputActions;
+    // New input system doesn't have continous input on mouse and keyboard.
+    // So player is required to spam WASD - which is not what I want.
+    // The only way I see to bypass this issue is by hand coding
+    // all inputs through this script.
 
-    public float moveSpeed = 1f;
+    //public InputActionReference myTest;
+
+    public float moveSpeed = 5f;
     
     private Rigidbody2D rb;
-    private bool playerIsMoving;
     
     private void Start()
     {
@@ -20,21 +24,63 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        
+        PlayerMove();
     }
 
-    public void Move(InputAction.CallbackContext context)
+    void PlayerMove()
     {
-    
-        var targetPos = new Vector2(transform.position.x, transform.position.y);
-        var moveVector = context.ReadValue<Vector2>();
-    
-        targetPos += (moveVector * moveSpeed) * Time.deltaTime;
-    
-        rb.MovePosition(targetPos);
-     
-        Debug.Log(moveVector);
-    
+        Vector2 temp = new Vector2(transform.position.x, transform.position.y);
+        //Vector2 targetPos;
+
+        if (Keyboard.current.wKey.isPressed)
+        {
+            // Move player up
+            //targetPos = temp + (Vector2.up * moveSpeed);
+            //rb.MovePosition(targetPos + (Vector2.up * moveSpeed) * Time.deltaTime);
+        }
+
+        if (Keyboard.current.aKey.isPressed)
+        {
+            // Move player left
+            //targetPos += new Vector2(-1, 0);
+        }
+
+        if (Keyboard.current.sKey.isPressed)
+        {
+            // Move player down
+            //targetPos += new Vector2(0, -1);
+        }
+
+        if (Keyboard.current.dKey.isPressed)
+        {
+            // Move player right
+            //targetPos += new Vector2(1, 0);
+
+        }
+
+        //targetPos.Normalize();
+        //rb.MovePosition(targetPos);
+
+        //Debug.Log(targetMovePos);
+
     }
+
+    //public void Move(InputAction.CallbackContext context)
+    //{
+    //    // Keyboard and mouse.
+    //
+    //    var targetPos = new Vector2(transform.position.x, transform.position.y);
+    //    var moveVector = context.ReadValue<Vector2>();
+    //
+    //    targetPos += (moveVector * moveSpeed) * Time.deltaTime;
+    //
+    //    rb.MovePosition(targetPos);
+    //
+    //    //Move(context);
+    //
+    // 
+    //    //Debug.Log(moveVector);
+    //
+    //}
 
 }
